ef7a5895fd1f5baaf09d46b41740f021244d451c
Mohammed <moody.busaidi99@gmail.com>
2023-05-15 12:16:00 +0400

    first

---
+++ src/main/java/com/example/task/Measurement/Measurement.java
@@ -0,0 +1,14 @@
+package com.example.task.Measurement;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Data
+public class Measurement {
+    Long id;
+    char alphabet;
+    int number;
+}
---
+++ src/main/java/com/example/task/Measurement/MeasurementController.java
@@ -0,0 +1,4 @@
+package com.example.task.Measurement;
+
+public class MeasurementController {
+}
